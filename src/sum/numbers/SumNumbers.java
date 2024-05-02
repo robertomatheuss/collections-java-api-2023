84f5af53107a1b290e24417916d9cc5b418ad02f
Roberto Matheus <robertmatheusa@gmail.com>
2024-05-02 11:49:21 -0300

    Resolved: Sum Numbers

---
+++ src/sum/numbers/SumNumbers.java
@@ -0,0 +1,42 @@
+package sum.numbers;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+public class SumNumbers {
+    private List<Integer> intList;
+
+    public SumNumbers(){
+        intList = new ArrayList<>();
+    }
+
+    public void addNumber(Integer number){
+        intList.add(number);
+    }
+
+    public Integer calculateSum(){
+        return intList.stream().mapToInt(x->x).sum();
+    }
+
+    public Integer findBiggestNumber(){
+        Optional<Integer> number= intList.stream().max(Integer::compareTo);
+        if (number.isPresent())
+            return number.get();
+        else
+            throw new NoSuchElementException();
+    }
+
+    public Integer findSmallestNumber(){
+        Optional<Integer> number= intList.stream().min(Integer::compareTo);
+        if (number.isPresent())
+            return number.get();
+        else
+            throw new NoSuchElementException();
+    }
+
+    public List<Integer> getIntList() {
+        return intList;
+    }
+}
